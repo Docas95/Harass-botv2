2d54359fef70b1879a10e54184b9a0c96d694726
Docas95 <61094301+Docas95@users.noreply.github.com>
2021-07-19 21:45:06 +0000

    Create extras.py

---
+++ extras.py
@@ -0,0 +1,9 @@
+standardErrorMessage = " If you need any help with this (or other) bot commands use %help. If you believe there is something wrong with this bot, contact @docas#6345 ."
+
+helpText = 'Hello! Here\'s how to use all the Harass-bot commands. \n %help - explains what each command does and how to use them. \n %createevent <Title (one word only)> <Date YYYY-MM-DD> <Description> - adds a new event to the bot, the bot will store the name of the event, its event, and a description of the event (optionally). \n %deleteevent <Title> - deletes an event from the database. \n %eventlist - gives a list of all the events. \n %harassmentlevel small/normal/high/spamoverflow - changes the amount of time between spam messages. \n %definespamchannel <channel id> - defines which channel in the server the bot should use to spam. \n %bread - bread puns, that\'s it.'
+
+eventAlerts = [
+  "Hello @everyone! Harass-bot is here to remind you of what you have to do!\n",
+  "The world's best bot is here to spam your discord notifications! @everyone\n",
+  "Don't forget about these: @everyone \n"
+]
